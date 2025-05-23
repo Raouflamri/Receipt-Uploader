6bcc1068f1dbdef8bc31ef865dd6429acd9b396c
Raouflamri <abderaouf4melam@gmail.com>
2025-05-23 19:58:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -76,3 +76,9 @@ searchInput.addEventListener("input", filterReceipts);
 exportBtn.addEventListener("click", exportReceipts);
 
 loadReceipts();
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl = 'https://mywotdmfnuewctbanedi.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15d290ZG1mbnVld2N0YmFuZWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMjA5ODIsImV4cCI6MjA2MzU5Njk4Mn0.81yT4CRzXsHvqFh7g_DE3dsXcqRAN-gzai5KEStXPvk';
+const supabase = createClient(supabaseUrl, supabaseKey);
