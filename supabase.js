b8b65181c1632b546d4e759a51aa7f77087c1ace
Raouflamri <abderaouf4melam@gmail.com>
2025-05-23 18:53:13 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://YOUR-PROJECT-ID.supabase.co";
-const SUPABASE_KEY = "YOUR-ANON-KEY";
+const SUPABASE_URL = "https://mywotdmfnuewctbanedi.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15d290ZG1mbnVld2N0YmFuZWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMjA5ODIsImV4cCI6MjA2MzU5Njk4Mn0.81yT4CRzXsHvqFh7g_DE3dsXcqRAN-gzai5KEStXPvk";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
